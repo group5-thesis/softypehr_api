7ee88af0b246eaab1faae0f4e2d94bc36693295d
LloryGlaus422 <lorly.sugal@student.passerellesnumeriques.org>
2020-10-10 10:47:04 +0800

    initial

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,13 +10,9 @@ class PagesController extends Controller
     public function index(){
         return view('index');
       }
-   
       public function uploadFile(Request $request){
-   
-        if ($request->input('submit') != null ){
-   
+         if ($request->input('submit') != null ){
            $file = $request->file('file');
-   
            // File Details
            $filename = $file->getClientOriginalName();
            $extension = $file->getClientOriginalExtension();
@@ -24,33 +20,30 @@ class PagesController extends Controller
            $fileSize = $file->getSize();
            $mimeType = $file->getMimeType();
    
-           // Valid File Extensions
-           $valid_extension = array("jpg","jpeg","png","txt","pdf","docs");
-   
+           // file distinction
+           $image_extension = array("jpg","jpeg","png");
+           $docs_extension = array("pdf","docs","txt");
            // 2MB in Bytes
            $maxFileSize = 2097152;
    
            // Check file extension
-           if(in_array(strtolower($extension),$valid_extension)){
-   
-             // Check file size
+           if(in_array(strtolower($extension),$image_extension)){
              if($fileSize <= $maxFileSize){
-   
-                // File upload location
                 $location = 'images';
-   
-                // Upload file
                 $file->move($location,$filename);
-                
-                Session::flash('message','Upload Successful.');
              }else{
-                Session::flash('message','File too large. File must be less than 2MB.');
+                alert('File too large. File must be less than 2MB.');
              }
-   
-           }else{
-              Session::flash('message','Invalid File Extension.');
-           }
-   
+           }elseif(in_array(strtolower($extension),$docs_extension)){
+            if($fileSize <= $maxFileSize){
+               $location = 'documents';
+               $file->move($location,$filename);
+            }else{
+               alert('File too large. File must be less than 2MB.');
+            }
+          }else{
+            alert('file not valid');
+          }
          }
    
          // Redirect to index
