dbda4b8d3d842128a7141d1b200797b4af1b8d50
LloryGlaus422 <lorly.sugal@student.passerellesnumeriques.org>
2020-10-17 15:14:38 +0800

    FileUpload Update

--- routes/api.php
+++ routes/api.php
@@ -49,11 +49,13 @@ Route::group(['middleware' => 'api-header'], function () {
     // File upload
     // Route::get('/image/{folder}/{file}','FileController@serve');
     // Route::post('/upload','FileController@store');
+    // Route::get('/retrieveLimitedFiles/{id}','FileController@retrieveLimitedFile');
     Route::post('/add_file','FileController@addFile');
     Route::get('/retrieve_files','FileController@retrieveFiles');
-    Route::get('/retrieveLimitedFiles/{id}','FileController@retrieveLimitedFile');
-    Route::post('/retrieveFilesByType','FileController@retrieveFilesByType');
-    Route::post('/delete_file','FileController@deleteFile');
+    Route::get('/retrieve_files_by_type/{id}','FileController@retrieveFilesByType');
+    Route::post('/delete_file/{id}','FileController@deleteFile');
+    Route::post('/update_file','FileController@updateFile');
+
 
     // Employee
     Route::post('/create_employee', 'EmployeeController@createEmployee');
@@ -61,7 +63,7 @@ Route::group(['middleware' => 'api-header'], function () {
     Route::get('/retrieve_limited_employee/{id}','EmployeeController@retrieveLimitedEmployee');
     Route::get('/retrieve_employee_by_department/{id}','EmployeeController@retrieveEmployeeByDepartment');
     Route::get('/retrieve_employee_by_manager/{id}','EmployeeController@retrieveEmployeeByManager');
-    Route::post('/delete_employee/{id}','EmployeeController@deleteEmpl  oyee');
+    Route::post('/delete_employee/{id}','EmployeeController@deleteEmployee');
     Route::post('/update_employee','EmployeeController@updateEmployee');
     Route::post('/retrieve_employee_profile','EmployeeController@retrieveEmployeeProfile');
 
